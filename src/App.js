cf63dcd48140b574bdec6953875dbcb8c3262b68
AnSui22 <ansui218@ewhain.net>
2024-01-04 12:19:34 +0900

    feat: Page Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import RouteTest from "./components/RouteTest";
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
