bffdd3002c4846818f639f121346fc6841f59768
AnSui22 <ansui218@ewhain.net>
2024-02-01 02:46:40 +0900

    feat: 다이어리 상세페이지 보기 기능 추가

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,13 +1,88 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import { getStringDate } from "../util/date";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { emotionList } from "../util/emotionList";
 
 const Diary = () => {
   const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
 
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 Diary 입니다.</p>
-    </div>
-  );
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        setData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">loading...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={"HOME"}
+          leftChild={<MyButton text={"<"} onClick={() => navigate(-1)} />}
+          rightChild={
+            <MyButton
+              text={"EDIT"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+        <article>
+          <section>
+            <div className="diary_date_wrapper">
+              <p>{getStringDate(new Date(data.date))}</p>
+            </div>
+          </section>
+
+          <section>
+            <div className="diary_title_wrapper">
+              <p>{data.title}</p>
+            </div>
+          </section>
+
+          <section>
+            <p>Mood</p>
+            <div className="diary_img_wrapper">
+              <img src={curEmotionData.emotion_img} />
+              <div className="emotion_descript">
+                {curEmotionData.emotion_descript}
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Today's PlayList</h2>
+            <div className="diary_music_wrapper">
+              <p>{data.artist}</p>
+              <p>{data.song}</p>
+            </div>
+          </section>
+
+          <section>
+            <h2>Today's Diary</h2>
+            <div className="diary_content_wrapper">
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
+      </div>
+    );
+  }
 };
 export default Diary;
