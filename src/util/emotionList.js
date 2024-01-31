d558b5c4606c35723e80d23677f52d1aa34181db
AnSui22 <ansui218@ewhain.net>
2024-02-01 02:47:57 +0900

    perf: 자주 쓰는 것들 따로 빼서 util 생성

---
+++ src/util/emotionList.js
@@ -0,0 +1,32 @@
+export const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "average",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "happy",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "sad",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "annoyed",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "nervous",
+  },
+  {
+    emotion_id: 6,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion6.png`,
+    emotion_descript: "tired",
+  },
+];
