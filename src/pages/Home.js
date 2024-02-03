8e8fb594f4c0000efbd0e30157281f2c13da4107
AnSui22 <ansui218@ewhain.net>
2024-02-03 15:24:53 +0900

    feat: Calender 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,97 @@
 import { useState, useContext, useEffect } from "react";
 import { DiaryStateContext } from "../App";
 
+import { format } from "date-fns";
+import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from "date-fns";
+import { isSameMonth, isSameDay, addDays, parse } from "date-fns";
+
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import DiaryList from "../components/DiaryList";
+
+const RenderDays = () => {
+  const week = [];
+  const date = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  for (let i = 0; i < 7; i++) {
+    week.push(
+      <div className="col" key={i}>
+        {date[i]}
+      </div>
+    );
+  }
+
+  return <div className="week_row">{week}</div>;
+};
+
+const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = "";
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = format(day, "d");
+      const cloneDay = day;
+      days.push(
+        <div
+          className={`col cell ${
+            !isSameMonth(day, monthStart)
+              ? "disabled"
+              : isSameDay(day, selectedDate)
+              ? "selected"
+              : format(currentMonth, "M") !== format(day, "M")
+              ? "not-valid"
+              : "valid"
+          }`}
+          key={day}
+          onClick={() => onDateClick(parse(cloneDay))}
+        >
+          <span
+            className={
+              format(currentMonth, "M") !== format(day, "M")
+                ? "text not-valid"
+                : ""
+            }
+          >
+            {formattedDate}
+          </span>
+        </div>
+      );
+      day = addDays(day, 1);
+    }
+    rows.push(
+      <div className="day_row" key={day}>
+        {days}
+      </div>
+    );
+    days = [];
+  }
+  return <div className="body">{rows}</div>;
+};
 
 const Home = () => {
   const diaryList = useContext(DiaryStateContext);
 
   const [data, setData] = useState([]);
   const [curDate, setCurDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const onDateClick = (day) => {
+    setSelectedDate(day);
+  };
 
   const currentYear = new Date().getFullYear();
   const years = Array.from({ length: 5 }, (_, i) => currentYear - 2 + i);
   const months = Array.from({ length: 12 }, (_, i) => i + 1);
 
-  const headText = `${curDate.getFullYear()} / ${curDate.getMonth() + 1}`;
+  const headText = `${curDate.getFullYear()} ${curDate.getMonth() + 1}`;
 
   useEffect(() => {
     if (diaryList.length >= 1) {
@@ -38,14 +114,10 @@ const Home = () => {
   }, [diaryList, curDate]);
 
   const increaseMonth = () => {
-    setCurDate(
-      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
-    );
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 1));
   };
   const decreaseMonth = () => {
-    setCurDate(
-      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
-    );
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, 1));
   };
 
   const handleYearChange = (e) => {
@@ -98,6 +170,15 @@ const Home = () => {
         leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
         rightChild={<MyButton text={">"} onClick={increaseMonth} />}
       />
+
+      <div className="calender">
+        <RenderDays />
+        <RenderCells
+          currentMonth={curDate}
+          selectedDate={selectedDate}
+          onDateClick={onDateClick}
+        />
+      </div>
 
       <DiaryList diaryList={data} />
     </div>
