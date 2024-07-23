58fbe480aefb895a21c5fe95dcf2c559e8bbe1e4
AnSui22 <ansui218@ewhain.net>
2024-07-24 00:02:01 +0200

    style: 다이어리 상세보기 스타일 변경

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -5,6 +5,8 @@ import { getStringDate } from "../util/date";
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import { weatherList } from "../util/weatherList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeadphones } from "@fortawesome/free-solid-svg-icons";
 
 const Diary = () => {
   const { id } = useParams();
@@ -43,39 +45,40 @@ const Diary = () => {
             />
           }
         />
-        <article>
-          <section>
-            <div className="diary_title_wrapper">
-              <p>{data.title}</p>
-            </div>
-          </section>
+        <article className="diary_wrapper">
+          <div className="img_wrapper">
+            <p>/ {data.title} /</p>
+          </div>
 
-          <section>
-            <h2>Weather</h2>
-            <div className="diary_img_wrapper">
-              <img src={curWeatherData.weather_img} />
+          <div className="info_wrapper">
+            <div className="date_wrappper">
+              <h2>Date</h2>
               <div className="emotion_descript">
-                {curWeatherData.weather_descript}
+                {getStringDate(new Date(data.date))}
               </div>
             </div>
-          </section>
 
-          <section>
-            <h2>Today's PlayList</h2>
-            <div className="diary_music_wrapper">
-              <p>
-                {data.artist} - {data.music}
-              </p>
+            <div className="date_wrappper2">
+              <h2>Weather</h2>
+              <div className="diary_img_wrapper">
+                <img src={curWeatherData.weather_img} />
+              </div>
             </div>
-          </section>
-
-          <section>
-            <h2>Today's Diary</h2>
-            <div className="diary_content_wrapper">
-              <p>{data.content}</p>
-            </div>
-          </section>
+          </div>
         </article>
+
+        <div className="song_wrappper">
+          <div className="diary_song_icon">
+            <FontAwesomeIcon icon={faHeadphones} />{" "}
+          </div>
+          <div className="emotion_descript">
+            {data.artist} - {data.music}
+          </div>
+        </div>
+
+        <div className="diary_content_wrapper">
+          <p>{data.content}</p>
+        </div>
       </div>
     );
   }
